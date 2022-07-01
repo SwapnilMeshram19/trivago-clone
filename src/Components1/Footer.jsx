bba81f59896f1e31a86c70cc98d31498914ce123
Mdshahbazalam6 <shahbazalam2002620@gmail.com>
2022-07-01 16:40:33 +0530

    improve navbar and footer

--- src/Components1/Footer.jsx
+++ src/Components1/Footer.jsx
@@ -1,76 +1,93 @@
 import React from 'react'
 import './NavbarFooter.css'
-function Footer(){  
-    return(
-        <>
-        <div className='Footer'>
-       <p style = {{marginLeft:240,marginBottom:-10,fontSize:13}}>trivago N.V., KesselstraBe 5-7,40221Dusseldorf</p>
-       <a href = "#" style={{marginLeft:-15,marginLeft:240}}>germany</a>
-       {/* <span> */}
-
-        <select className='SelectData'>
-           <option>India</option>
-           <option>India</option>
-            <option>India</option>
-            <option>Australia</option>
-            <option>Brazil</option>
-            <option>Arab</option>
-            <option>Chill</option>
-            <option>china</option>
-            <option>maharastra</option>
-            <option>Mumbai</option>
-            <option>Delhi</option>
-            <option>bihar</option>
-            <option>maharastra</option>
-            <option>AUstrali</option>
-            <option>Japan</option>
-            <option>Germany</option>
-            <option>Hang kong</option>
-            <option>Greece</option>
-            
-
-        </select>
-       {/* </span> */}
-       
-       <div className='Image'>
-        <div className='Image1'>
-       <img  className = "Image" src = "https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Twitter-logo-transparent-PNG.png"/>
-       <img className = "Image"  src = "https://i.pinimg.com/originals/41/e4/57/41e457e8599833299d563976b7d3c59a.jpg"/>
-       <img className = "Image"  src = "https://i.pinimg.com/originals/ee/93/32/ee933267f1290803739dd38bca4a9b25.jpg"/>
-       <img  className = "Image" src = "https://i.pinimg.com/originals/20/9b/d8/209bd859c265e7ffc4bfeb75877b23f7.png"/>
-      <img  className = "Image" src = "https://toppng.com/uploads/preview/linkedin-square-logo-comments-linkedin-icon-black-11563087424lpctodfamj.png"/>
-    
-      </div>
-      </div>
-      
-    <div className='footer'>
-        <div className='footer1'>
-      <a href = "#">Company</a>
-      <a href = "#">Jobs</a>
-      <a href = "#">Press</a>
-      <a href = "#">Investor relation</a>
-      </div>
-      <div className='footer2'>
-      <a href = "#">Mobile apps-searching on the go</a>
-      <a href = "#" style = {{marginBottom:90}}>trivago Business Studio</a>
-      </div>
-
-      <div className='footer3'>
-      <a href="#">Help</a>
-        <a href = "#">Learn how trivago works</a>
-        <a href = "#">Terms and conditions</a>
-        <a href = "#">Legal information Do Not Sell My Personal Information</a>
-        <a href = "#">Private notice</a>
-        <a href = "#">Cyber Security</a>
-        <a href = "">Advice</a>
-
-      </div>
-     </div>
-      <div><h1 style = {{textAlign:'center'}}>trivago</h1>
-      <p style = {{fontSize:12,marginTop:-10,textAlign:'center'}}>Copyright 2022 trivago | All right reserved</p>
+function Footer() {
+  return (
+    <>
+      <div className='Footer'>
+        <div className='Footer_first_child'>
+          <div><p>Want to receive exclusive hotel offers? Subscribe to our newsletter!</p></div>
+          <div>
+            <div><input type="text" placeholder='Email address' /></div>
+            <div><button>Subscribe</button></div>
+          </div>
         </div>
+        <hr className='footerhr'/>
+        <div className="footerSelectBox">
+          <div>
+            <p ><b>trivago N.V.,</b> KesselstraBe 5-7,40221 Dusseldorf,</p>
+            <p>Germany</p>
+          </div>
+          {/* <span> */}
+
+          <div>
+            <select className='SelectData'>
+              <option>India</option>
+              <option>India</option>
+              <option>India</option>
+              <option>Australia</option>
+              <option>Brazil</option>
+              <option>Arab</option>
+              <option>Chill</option>
+              <option>china</option>
+              <option>maharastra</option>
+              <option>Mumbai</option>
+              <option>Delhi</option>
+              <option>bihar</option>
+              <option>maharastra</option>
+              <option>AUstrali</option>
+              <option>Japan</option>
+              <option>Germany</option>
+              <option>Hang kong</option>
+              <option>Greece</option>
+
+
+            </select>
+          </div>
         </div>
-        </>
-    )
+        {/* </span> */}
+
+
+        <div className='Image'>
+          <div className='Image1'>
+            <a href="https://twitter.com/trivago" style={{ textDecoration: 'none' }} ><img className="Image" src="https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Twitter-logo-transparent-PNG.png" /></a>
+            <a href="https://www.instagram.com/trivago/" style={{ textDecoration: 'none' }}> <img className="Image" src="https://i.pinimg.com/originals/41/e4/57/41e457e8599833299d563976b7d3c59a.jpg" /></a>
+            <a href="https://www.facebook.com/trivago"> <img className="Image" src="https://i.pinimg.com/originals/ee/93/32/ee933267f1290803739dd38bca4a9b25.jpg" /></a>
+            <a href="https://www.linkedin.com/company/trivagogmbh/" style={{ textDecoration: 'none' }}> <img className="Image" src="https://i.pinimg.com/originals/20/9b/d8/209bd859c265e7ffc4bfeb75877b23f7.png" /></a>
+
+            <a href="#" style={{ textDecoration: 'none' }}><img className="Image" src="https://toppng.com/uploads/preview/linkedin-square-logo-comments-linkedin-icon-black-11563087424lpctodfamj.png" /></a>
+
+          </div>
+        </div>
+
+        <div className='footer'>
+          <div className='footer1'>
+            <a href="#">Company</a><br />
+            <a href="#">Jobs</a><br />
+            <a href="#">Press</a><br />
+            <a href="#">Investor relation</a>
+          </div>
+          <div className='footer2'>
+            <a href="#">Mobile apps-searching on the go</a><br />
+            <a href="#" style={{ marginBottom: 90 }}>trivago Business Studio</a>
+          </div>
+
+          <div className='footer3'>
+            <a href="#">Help</a><br />
+            <a href="#">Learn how trivago works</a><br />
+            <a href="#">Terms and conditions</a><br />
+            <a href="#">Legal information</a><br />
+            <a href="#"> Do Not Sell My Personal Information</a>
+            <a href="#">Private notice</a><br />
+            <a href="#">Cyber Security</a><br />
+            <a href="">Advice</a>
+
+          </div>
+        </div>
+        <div className='footerCopyrightBox'><h1 style={{ textAlign: 'center' }}>trivago</h1>
+          <p >Copyright 2022 trivago | All right reserved</p>
+        </div>
+      </div>
+    </>
+  )
 }
 export { Footer }
--- src/Components1/Navbar.jsx
+++ src/Components1/Navbar.jsx
@@ -1,68 +1,60 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 // import styled from 'styled-components'
-import { DropDown } from './DropDown'
 import './NavbarFooter.css';
 
 
- function Navbar(){
-    return(
-        <>
-    
-        <div className='navbar'>
-            <div className='navbar1'>
-         <Link to = "#" ><img style ={{width:100,marginTop:-40,marginRight:190,marginLeft:-100}} src = "https://imgcy.trivago.com/f_auto,q_auto/hardcodedimages/logo_fb.png"/></Link>
-         <span  style = {{marginRight:-13}}class="material-symbols-outlined">king_bed</span>
-        <Link to = "Stays" style = {{color:'black'}}>Stays</Link>
-        <span  style = {{marginRight:-13}} class="material-symbols-outlined">electric_car</span>
-        <Link to = "Cars" style = {{color:'black'}}>Cars</Link>
-        <span  style = {{marginRight:-13}} class="material-symbols-outlined">flight</span>
-        <Link to = "Flights" style = {{color:'black'}}>Flights</Link>
-        <span style = {{marginRight:-13}} class="material-symbols-outlined">inventory_2</span>
-        <Link to = "Packages" style = {{color:'black'}}>Packages</Link>
-        
-            {/* <div className='login'> */}
-            <Link to ="login" style={{color:'black',marginLeft:550}}>Log in</Link>
-            
-            <DropDown />
-            
-                {/* <div className='Scroll'> */}
-            <select style = {{height:25,width:50,marginLeft:0,outline:'none',border:'none'}}>EUR
-            
-                {/* <div className='Scroll1'> */}
-                <option>EUR</option>
-                <option>GBP</option>
-                <option>USD</option>
-                <option>AED</option>
-                <option>ARS</option>
-                <option>AUD</option>
-                <option>AZN</option>
-                <option>BGN</option>
-                <option>BHD</option>
-                <option>BBR</option>
-                <option>CAD</option>
-                <option>AHF</option>
-                <option>CLP</option>
-                <option>ANY</option>
-                <option>COP</option>
-                <option>CZK</option>
-                <option>COP</option>
-                <option>CZK</option>
-                <option>DKK</option>
-                <option>CZK</option>
-                <option>DZD</option>
-                <option>EGP</option>
-                <option>EUR</option>
-                <option>HKD</option>
-                {/* </div> */}
-                {/* </div> */}
-            </select>
-            {/* </div> */}
-            
-        </div>
-    
-    </div>
-        </>
+
+function Navbar() {
+    const [menue, setMenue] = useState(false)
+    console.log(menue)
+    return (
+        < div className='navbar'>
+
+
+                <div className="NavbarFirstChild">
+
+                    <Link to="/" ><img className='NavbarImage' src="https://tse2.mm.bing.net/th?id=OIP.JSGmiGzRmDowBYk0KLyJAwHaCO&pid=Api&P=0&w=499&h=149" alt="" /></Link>
+                </div>
+                <div className="NavbarSecondChild">
+                    <div> <Link to="login" >Log in</Link></div>
+                    <div onMouseEnter={() => setMenue(true)} onMouseLeave={() => setMenue(false)}><p>Menue </p></div>
+                    {menue ?( <div className="navbarDropdownBox" onMouseEnter={() => setMenue(true)} onMouseLeave={() => setMenue(false)}>
+            <a href=""><p>Recently View</p></a>
+            <a href=""> <p>Booking View</p></a>
+            <a href=""><p>Help and Support</p></a>
+            </div>) : ''}
+                    <div>
+                        <select name="" id="" style={{width:'5vw',border:'none'}}>
+                            <option>EUR</option>
+                            <option>GBP</option>
+                            <option>USD</option>
+                            <option>AED</option>
+                            <option>ARS</option>
+                            <option>AUD</option>
+                            <option>AZN</option>
+                            <option>BGN</option>
+                            <option>BHD</option>
+                            <option>BBR</option>
+                            <option>CAD</option>
+                            <option>AHF</option>
+                            <option>CLP</option>
+                            <option>ANY</option>
+                            <option>COP</option>
+                            <option>CZK</option>
+                            <option>COP</option>
+                            <option>CZK</option>
+                            <option>DKK</option>
+                            <option>CZK</option>
+                            <option>DZD</option>
+                            <option>EGP</option>
+                            <option>EUR</option>
+                            <option>HKD</option>
+                        </select>
+                    </div>
+                </div>
+        </ div>
     )
 }
 export { Navbar }
